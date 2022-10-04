b4920becfc5f74c38a692fab8a2b7a26c44a0d3e
ArunpandiAP <arunpandi241@gmail.com>
2022-10-05 03:06:37 +0530

    arun code

--- src/test/java/org/test/FbOpen.java
+++ src/test/java/org/test/FbOpen.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FbOpen {
-	public static void main (String []arges) {
+	public static void main (String []arges) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "C:\\eclipse\\javaproject\\MavenClass\\Driver\\chromedriver.exe");
 				
 		WebDriver driver=new ChromeDriver();
@@ -15,8 +15,10 @@ public class FbOpen {
 		driver.manage().window().maximize();
 		WebElement txtEmail=driver.findElement(By.id("email"));txtEmail.sendKeys("arun@gmail.com");
 		WebElement txtPass=driver.findElement(By.id("pass"));txtPass.sendKeys("12345678");
+		Thread.sleep(2000);
 		WebElement btnClick=driver.findElement(By.name("login"));btnClick.click();
-				
+	
+		
 	}
 
 }
